5a075b3229ff7647450711d4ef525e29e82d8e48
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-10-17 05:23:30 +0100

    added default value for verified and admin column on users migration file

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\User;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+
 
 class CreateUsersTable extends Migration
 {
@@ -20,9 +22,9 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('verified');
-            $table->string('verification_token');
-            $table->string('admin');
+            $table->string('verified')->default(User::UNVERIFIED_USER);
+            $table->string('verification_token')->nullable();
+            $table->string('admin')->default(User::REGULAR_USER);
             $table->timestamps();
         });
     }
