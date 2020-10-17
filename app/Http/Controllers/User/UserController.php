6d3fbc14c6dd59bbdd6539e62b245f6f8555704f
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-10-17 18:31:38 +0100

    delete user end-point added

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -143,10 +143,15 @@ class UserController extends Controller
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return response()->json(['data' => $user], 200);
+
     }
 }
