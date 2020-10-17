8e3988e8f91c089add5aa7c49db3adbd61f81087
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-10-17 14:28:28 +0100

    added Product factory in the Usherfactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 use  App\User;
 use  App\Category;
+use  App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,9 +33,27 @@ $factory->define(User::class, function (Faker $faker) {
 });
 
 
+/**
+ * Category Factory
+ */
 $factory->define(Category::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
+    ];
+});
+
+/**
+ * Product Factory
+ */
+$factory->define(Product::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1, 10),
+        'status' => $faker->randomElement([Product::AVAILABLE_PRODUCT, Product::UNAVAILABLE_PRODUCT]),
+        'image' => $faker->randomElement(['1.jpg', '2.jpg','3.jpg']),
+        'seller_id' => User::all()->random()->id,
+        //'seller_id' => User::inRandomOrder()->first()->id
     ];
 });
