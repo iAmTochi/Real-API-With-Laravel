6c947142d1dbd6d892f2a9071c13758c95ee9173
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-10-28 14:59:45 +0100

    Implementing the index method for BuyerCategoryController

--- app/Http/Controllers/Buyer/BuyerCategoryController.php
+++ app/Http/Controllers/Buyer/BuyerCategoryController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Buyer;
 use App\Buyer;
 use App\Http\Controllers\ApiController;
 
-class BuyerSellerController extends ApiController
+class BuyerCategoryController extends ApiController
 {
     /**
      * Display a listing of the resource.
@@ -14,13 +14,15 @@ class BuyerSellerController extends ApiController
      */
     public function index(Buyer $buyer)
     {
-        $sellers = $buyer->transactions()->with('product.seller')
-            ->get()->pluck('product.seller')
+        $categories = $buyer->transactions()->with('product.categories')
+            ->get()
+            ->pluck('product.categories')
+            ->collapse()
             ->unique('id')
             ->values();
 
-        return $this->showAll($sellers);
-
+        return $this->showAll($categories);
     }
+
 
 }
