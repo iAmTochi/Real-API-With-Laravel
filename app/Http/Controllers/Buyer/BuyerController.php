86b717b62470378bf15d6983bd1422587a42a863
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-10-28 01:48:06 +0100

    added buyerScope using globalScope

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -28,9 +28,9 @@ class  BuyerController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\JsonResponse
      */
-    public function show($id)
+    public function show(Buyer $buyer)
     {
-        $buyer = Buyer::has('transactions')->findOrFail($id);
+        //$buyer = Buyer::has('transactions')->findOrFail($id);
 
         return $this->showOne($buyer);
     }
