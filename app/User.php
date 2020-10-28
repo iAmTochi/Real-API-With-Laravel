76f048b153d662505a0d137152d7eb1a1d4bb610
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-10-28 02:23:35 +0100

    added softDeletes to user, product, transaction, and category model

--- app/User.php
+++ app/User.php
@@ -2,13 +2,15 @@
 
 namespace App;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
+
 
     const VERIFIED_USER = '1';
     const UNVERIFIED_USER = '0';
@@ -16,6 +18,7 @@ class User extends Authenticatable
     const ADMIN_USER = 'true';
     const REGULAR_USER = 'false';
 
+    protected $dates = ['deleted_at'];
     protected $table = 'users';
 
 
