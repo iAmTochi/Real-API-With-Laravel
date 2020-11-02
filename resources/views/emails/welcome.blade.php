d579d9dc6ff621a5fce2383865544466f16aa670
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-02 11:08:50 +0100

    restricting sending a mail to dummy email address while migrating data in the DatabaseSeeder.php, Flushing eventlisters for User model

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -2,4 +2,4 @@ Hello {{ $user->name }}
 
 Thank you for creating an account. Please verify your email address using this link;
 
-{{ route('verify', $user->verification_token) }}
+{{ route('verify', $user->verification_token ==null ? 'already-verified':$user->verification_token) }}
