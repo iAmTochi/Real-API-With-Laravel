ffeb56ee4ada3484b1fe7bed465f71e69029ba33
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-02 09:57:20 +0100

    implementing the user verification

--- routes/api.php
+++ routes/api.php
@@ -68,3 +68,4 @@ Route::resource('transactions.sellers', 'Transaction\TransactionSellerController
  * Users
  */
 Route::resource('users', 'User\UserController',['except' => ['create','edit']]);
+Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
