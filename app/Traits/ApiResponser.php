1f4930c2112b1817a05f2444f50e32925d0be16e
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-03 11:36:53 +0100

    filtering responses base on the attributes

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -27,8 +27,11 @@ trait ApiResponser {
         }
 
         $transformer = $collection->first()->transformer;
+
+        $collection= $this->filterData($collection, $transformer);
         $collection= $this->sortData($collection, $transformer);
         $collection= $this->transformData($collection, $transformer);
+
         return $this->successResponse($collection, $code);
     }
 
@@ -50,7 +53,20 @@ trait ApiResponser {
         return $this->successResponse(['data'=> $message], $code);
     }
 
+    protected function filterData(Collection $collection, $transformer){
+        foreach (request()->query() as $query => $value){
+            $attribute = $transformer::originalAttribute($query);
+
+            if (isset($attribute, $value)){
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+
+        return $collection;
+    }
+
     protected function sortData(Collection $collection, $transformer){
+
         if(request()->has('sort_by')){
             $attribute = $transformer::originalAttribute(request()->sort_by);
 
