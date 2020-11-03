f0da9b37d3e84a994721e48c0e11f532102a5536
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-03 16:45:55 +0100

    The caching system modified better

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -118,7 +118,15 @@ trait ApiResponser {
     protected function cacheResponse($data){
         $url = request()->url();
 
-        return Cache::remember($url,30, function () use($data){
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl,30, function () use($data){
             return $data;
         });
     }
