2d4dddc5c98fe17375b7b5251eaf2169b2b6111f
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-03 15:29:46 +0100

    paginating the showAll collections in ApiResponser.php - adding and implementing the paginate method

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,6 +3,7 @@
 namespace  App\Traits;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
 trait ApiResponser {
@@ -28,9 +29,10 @@ trait ApiResponser {
 
         $transformer = $collection->first()->transformer;
 
-        $collection= $this->filterData($collection, $transformer);
-        $collection= $this->sortData($collection, $transformer);
-        $collection= $this->transformData($collection, $transformer);
+        $collection = $this->filterData($collection, $transformer);
+        $collection = $this->sortData($collection, $transformer);
+        $collection = $this->paginate($collection);
+        $collection = $this->transformData($collection, $transformer);
 
         return $this->successResponse($collection, $code);
     }
@@ -74,6 +76,23 @@ trait ApiResponser {
         }
 
         return $collection;
+    }
+
+    protected function paginate(Collection $collection){
+        $page = LengthAwarePaginator::resolveCurrentPage();
+
+        $perPage = 15;
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+
+        $paginated->appends(request()->all());
+
+        return $paginated;
+
     }
 
     protected function transformData($data, $transformer){
