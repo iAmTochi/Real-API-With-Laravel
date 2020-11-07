da1bc96bf4c3b844eea7ec8c24639f1c79b2a830
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-07 19:05:20 +0100

    modifying oauth/token route to use api middleware

--- routes/api.php
+++ routes/api.php
@@ -70,3 +70,9 @@ Route::resource('transactions.sellers', 'Transaction\TransactionSellerController
 Route::resource('users', 'User\UserController',['except' => ['create','edit']]);
 Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
 Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
+
+
+/**
+ * Passport
+ */
+Route::post('oauth/token', '\Laravel\Passport\Http\Controllers\AccessTokenController@issueToken');
