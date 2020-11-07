6842c487a7cad2df7e4c9e5e8fb745bfd81a7fe5
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-07 18:55:33 +0100

    making users authenticatable through api tokens

--- app/User.php
+++ app/User.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use Notifiable, SoftDeletes;
+    use Notifiable, HasApiTokens, SoftDeletes;
 
 
     const VERIFIED_USER = '1';
